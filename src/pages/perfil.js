0cb1d4f4e70ee8ddba8e14528c3d2b212cf77aec
ZeckBorges <quielborges@hotmail.com>
2023-10-29 00:09:20 -0300

    Criando branch perfil

--- src/pages/perfil.js
+++ src/pages/perfil.js
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'lightblue',
+    backgroundColor: 'darkblue',
   },
   username: {
     fontSize: 24,
